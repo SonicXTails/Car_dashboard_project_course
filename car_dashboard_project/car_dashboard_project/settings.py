e436bd13f584f7690effb7eef8c18036cf487ab1
SonicXTails <aedgy.master@mail.ru>
2025-09-06 11:24:09 +0300

    fix: Исправлен статический URL к статичным файлам (static/img и static/style)

--- car_dashboard_project/car_dashboard_project/settings.py
+++ car_dashboard_project/car_dashboard_project/settings.py
@@ -144,7 +144,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # если у тебя папка static в корне проекта
+]
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
